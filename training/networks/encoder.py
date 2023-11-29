22112b00b4370b33c260bcb904470a346e032396
jacob33 <hockeyviolin@gmail.com>
2023-11-29 00:00:29 -0500

    fixed emcpder

--- training/networks/encoder.py
+++ training/networks/encoder.py
@@ -14,9 +14,6 @@ class BrainScanEmbedder(nn.Module):
 
         self.encoder = EncodeBS(num_output_channels=upscale_schedule[0])
         module = []
-        encoder_layer = nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead)
-        module.append(nn.TransformerEncoder(encoder_layer, num_layers=model_config.num_transformer_layers))
-
         
         for i in range(1, len(upscale_schedule)):
             module.append(DoubleConv(upscale_schedule[i-1], upscale_schedule[i]))
@@ -24,11 +21,13 @@ class BrainScanEmbedder(nn.Module):
         module.append(DoubleConv(upscale_schedule[-1], upscale_schedule[-1], activation=False))
         self.backbone = nn.Sequential(*module)
         
+        encoder_layer = nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead)
+        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=model_config.num_transformer_layers)
 
     def forward(self, source, targets=None):
         output = self.encoder(source)
-        output = self.backbone(output)
         output = self.transformer_encoder(output)
+        output = self.backbone(output)
         if targets is None:
             return output
         loss = F.mse_loss(output, targets)
