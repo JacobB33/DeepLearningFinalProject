e7434a2a097741cf335b126bcc8f4548bbab6e86
jacob33 <hockeyviolin@gmail.com>
2023-11-29 02:23:47 -0500

    new model

--- training/networks/encoder.py
+++ training/networks/encoder.py
@@ -17,16 +17,17 @@ class BrainScanEmbedder(nn.Module):
         
         for i in range(1, len(upscale_schedule)):
             module.append(DoubleConv(upscale_schedule[i-1], upscale_schedule[i]))
+            module.append(nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead))
             module.append(ResBlock(upscale_schedule[i]))
         module.append(DoubleConv(upscale_schedule[-1], upscale_schedule[-1], activation=False))
         self.backbone = nn.Sequential(*module)
         
-        encoder_layer = nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=model_config.num_transformer_layers)
+        # encoder_layer = nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead)
+        # self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=model_config.num_transformer_layers)
 
     def forward(self, source, targets=None):
         output = self.encoder(source)
-        output = self.transformer_encoder(output)
+        # output = self.transformer_encoder(output)
         output = self.backbone(output)
         if targets is None:
             return output
