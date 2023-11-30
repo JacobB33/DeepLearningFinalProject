908c2fddcb5b0bb2d46165eb8c6806930b398dbe
jacob33 <hockeyviolin@gmail.com>
2023-11-30 18:51:35 -0500

    Added positional encoding

--- training/networks/encoder.py
+++ training/networks/encoder.py
@@ -6,7 +6,7 @@ import os
 from training.configs import ModelConfig
 from training.networks.building_blocks import ResBlock, DoubleConv, EncodeBS
 from training.networks.fancy_building_blocks import AttnBlock, ResnetBlock
-
+import math
 
 class plzWork(nn.Module):
     def __init__(self, model_config: ModelConfig) -> None:
@@ -31,7 +31,26 @@ class plzWork(nn.Module):
         loss = F.mse_loss(output, targets)
         return output, loss
         
-        
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        position = torch.arange(max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_len, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """
+        Arguments:
+            x: Tensor, shape ``[seq_len, batch_size, embedding_dim]``
+        """
+        x = x + self.pe[:x.size(0)]
+        return self.dropout(x)
 
 class BrainScanEmbedder(nn.Module):
     """This needs to be batched input of channel, brainscan or (c, 7604). Outputs (c, 77, 1024)"""
@@ -44,11 +63,11 @@ class BrainScanEmbedder(nn.Module):
         
         for i in range(1, len(upscale_schedule)):
             module.append(DoubleConv(upscale_schedule[i-1], upscale_schedule[i]))
-            module.append(nn.PositionalEncoding(d_model=1024, dropout=0))
+            module.append(PositionalEncoding(d_model=1024, dropout=0))
             module.append(nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead, dropout=0))
             # module.append(nn.Tanh())
             module.append(ResBlock(upscale_schedule[i]))
-        module.append(nn.PositionalEncoding(d_model=1024, dropout=0))
+        module.append(PositionalEncoding(d_model=1024, dropout=0))
         module.append(nn.TransformerEncoderLayer(d_model=1024, nhead=model_config.nhead, dropout=0))
         module.append(DoubleConv(upscale_schedule[-1], upscale_schedule[-1], activation=False))
         self.backbone = nn.Sequential(*module)
